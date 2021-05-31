3fc07139e27543503cdfc4e8108046f31a41987a
Tanish Baidya <tanish.baidya@gmail.com>
2021-05-31 22:28:41 +0530

    Testing optimisation for Dynamic Nav Bar Load time

--- js/main.js
+++ js/main.js
@@ -256,28 +256,9 @@
 
 })(jQuery);
 
-const nav_menu_template = '<li><a href="{{path}}">{{option}}</a></li>\n';
-
-var nav_menu_list = [];
-
-
-$.getJSON("/data/navigation-bar.json", function (data) {
-	nav_menu_list.push('<ul>\n')
-    $.each(data.navigation, function (index) {
-      var nav_menu = nav_menu_template ;
-      $.each(data.navigation[index], function (key, value) {
-        var expression = "{{" + key + "}}";
-        nav_menu = nav_menu.replace(expression, value);
-      });
-     nav_menu_list.push(nav_menu);
-    });
-	nav_menu_list.push('</ul>\n');
-	console.log();
-	$("#navigation-bar-list").html(nav_menu_list.join(' '));
-});
 
 
 
 
 
-console.log("done");
+
